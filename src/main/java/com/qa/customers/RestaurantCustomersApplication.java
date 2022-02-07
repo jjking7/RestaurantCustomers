4b93fd393a507f09dc97cd7736a03e1478fbfd46
jjking7 <jj_king9@hotmail.co.uk>
2022-02-07 10:41:15 +0000

    init commit

---
+++ src/main/java/com/qa/customers/RestaurantCustomersApplication.java
@@ -0,0 +1,13 @@
+package com.qa.customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantCustomersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/customers/RestaurantCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantCustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
